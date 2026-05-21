179797149321fb5b839822b1dc35c4972983d080
Abdul Basir <abdulbasir4211@gmail.com>
2026-05-21 13:14:34 +0500

    Add OPTIONS method to CORS configuration

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const app = express();
 
 app.use(cors({
   origin: "https://abdulbasir-portfolio.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
 
 app.options("*", cors());
 
